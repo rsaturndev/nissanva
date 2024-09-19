32bce032de48004b226180bc0c428ad3d53cf037
rsaturndev <rita.dragotta@saturnlounge.com>
2024-09-19 16:23:55 -0500

    Add'l nav work. Added js for dropdown functionality

--- nissanvalueadvantage/src/js/main.js
+++ nissanvalueadvantage/src/js/main.js
@@ -1,6 +1,6 @@
 // Import our custom CSS
 import '../scss/styles.scss'
-
+import '../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
 
 // Import all of Bootstrap's JS
-import * as bootstrap from 'bootstrap'
+import * as bootstrap from '../../node_modules/bootstrap';
