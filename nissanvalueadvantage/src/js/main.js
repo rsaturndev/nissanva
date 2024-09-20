60e350af7f5877da5c615e1b75f730c6e1b0735b
rsaturndev <rita.dragotta@saturnlounge.com>
2024-09-20 13:52:04 -0500

    Cont'd mobile menu adjustments

--- nissanvalueadvantage/src/js/main.js
+++ nissanvalueadvantage/src/js/main.js
@@ -4,3 +4,13 @@ import '../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
 
 // Import all of Bootstrap's JS
 import * as bootstrap from '../../node_modules/bootstrap';
+
+/* Fix to mobile toggle issue  */
+document.querySelector('.navbar-toggler').addEventListener('click', function() {
+    var menu = document.getElementById('navbarSupportedContent');
+    if (menu.classList.contains('show')) {
+        menu.classList.remove('show');
+    } else {
+        menu.classList.add('show');
+    }
+});
