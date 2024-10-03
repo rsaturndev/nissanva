540911fb5ee7a19ce2085e2327f592342f87ed98
rsaturndev <rita.dragotta@saturnlounge.com>
2024-10-03 10:49:52 -0500

    Start of navigation restructuring

--- nissanvalueadvantage/src/js/main.js
+++ nissanvalueadvantage/src/js/main.js
@@ -6,8 +6,17 @@ import '../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
 import * as bootstrap from '../../node_modules/bootstrap';
 
 /* Fix to mobile toggle issue  */
-document.querySelector('.navbar-toggler').addEventListener('click', function() {
+document.querySelector('.navbar-toggler-bottom').addEventListener('click', function() {
     var menu = document.getElementById('navbarSupportedContent');
+    if (menu.classList.contains('show')) {
+        menu.classList.remove('show');
+    } else {
+        menu.classList.add('show');
+    }
+});
+
+document.querySelector('.navbar-toggler-top').addEventListener('click', function() {
+    var menu = document.getElementById('navbarTopContent');
     if (menu.classList.contains('show')) {
         menu.classList.remove('show');
     } else {
